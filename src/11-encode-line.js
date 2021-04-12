ce2e236cd11e13e1677790e31a9cafb2fb6ea65f
Ilia Dvorchuk <vseprid@gmail.com>
2021-04-12 16:42:33 +0300

    feat: done three tasks

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const stack = [];
+  let count = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      count++;
+    } else if (count > 1) {
+      stack.push(`${count}${arr[i]}`);
+      count = 1;
+    } else {
+      stack.push(arr[i]);
+    }
+  }
+  return stack.join('');
 }
 
 module.exports = encodeLine;
